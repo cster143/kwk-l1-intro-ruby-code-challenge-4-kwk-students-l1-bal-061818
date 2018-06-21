959583d8b11ac0ffd11bfa3090ce958c2439adf0
cster143 <cster143@gmail.com>
2018-06-21 17:02:15 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,10 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
-end
+  coins_hash=
+    :quarters => 0,
+    :dimes => 0,
+    :pennies => 0
+}
+while cents<= 25
+  :quarters 
